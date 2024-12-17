7c5208a8a9312bb746db67762a52629f61bb5e05
GR212639 <ricardo.gonzalez@icbc.com,.>
2024-12-17 09:33:10 -0300

    modification

--- carusage/src/main/java/com/carusage/carusage/controller/CarController.java
+++ carusage/src/main/java/com/carusage/carusage/controller/CarController.java
@@ -5,16 +5,18 @@ import com.carusage.carusage.dto.CarDTO;
 import com.carusage.carusage.service.CarService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/car")
 public class CarController {
     private final CarService carService;
     public CarController(CarService carService) {
         this.carService = carService;
     }
 
-    @PostMapping(consumes = "text/plain")
+    @PostMapping
     public String createCar(@RequestBody CarDTO carUsage) {
 
         return carService.getCarByUsage(carUsage);
